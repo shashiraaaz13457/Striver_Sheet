d4b2ee86b38084bf3e8b1baf9e280f75b3f76ba2
Shashi raj <shashiraj13457@gmail.com>
2024-01-22 11:48:35 +0530

    Remove duplicate Elements from an Arrayf

--- Remove_duplicates_from_Array.java
+++ Remove_duplicates_from_Array.java
@@ -1,4 +1,25 @@
 package Array;
 
+import java.util.Scanner;
+
 public class Remove_duplicates_from_Array {
+    static int RemoveDuplicates(int[] nums){
+        int uniqueElement = 0 ;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i-1] != nums[i]) {
+                uniqueElement++;
+//                nums[uniqueElement] = nums[i];
+            }
+        }return uniqueElement +1;
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] nums = new int[5];
+        System.out.println("Enter values: ");
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = input.nextInt();
+        }
+        int result = RemoveDuplicates(nums);
+        System.out.println(result);
+    }
 }
