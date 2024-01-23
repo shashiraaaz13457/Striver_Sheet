cda80248b4ad5d1dfac4a9fa2ad3a3029dc79002
Shashi raj <shashiraj13457@gmail.com>
2024-01-23 21:41:20 +0530

    Rotate Array by k

---
+++ Rotate_Array_by_k.java
@@ -0,0 +1,27 @@
+package Array;
+
+import java.util.Scanner;
+
+public class Rotate_Array_by_k {
+    static int[] RotateArrayByK(int[] nums){
+        int n = nums.length;
+        int temp = nums[n-1];
+        for (int i = 0; i < nums.length; i++) {
+            if (i == n-1){
+                continue;
+            }
+            nums[i+1] = nums[i];
+        }
+        nums[0] = temp;
+        return nums;
+    }
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] nums = new int[5];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = input.nextInt();
+        }
+        System.out.println(RotateArrayByK(nums));
+    }
+}
