ade03d25ff8215b93b1dc1dea5902b1674ecda2f
Shashi raj <shashiraj13457@gmail.com>
2024-02-06 23:17:46 +0530

    Sort Colors

--- Medium/TwoSum.java
+++ Medium/TwoSum.java
@@ -1,12 +1,12 @@
 package Array.Medium;
-
+import java.util.Arrays;
 import java.lang.reflect.Array;
 
 public class TwoSum {
     public static void main(String[] args) {
-        int[] nums = new int[4];
+        int[] nums = {7,1,5,4};
         int target = 9;
-        int ans = twoSum(nums,target);
+        int[] ans = twoSum(nums,target);
         System.out.println(Arrays.toString(ans));
     }
 //    Input :- nums = [2,7,11,15] , target = 9;
@@ -14,13 +14,14 @@ public class TwoSum {
     static int[] twoSum(int[] nums,int target){
         int n = nums.length;
         int sum = 0;
-        int[] arr = new int [2];
+//        int[] arr = new int [2];
         for (int i = 0; i < n; i++) {
-            if (sum )
-            sum = sum + nums[i];
-            if (sum == target){
-                arr[0] = i;
+            for (int j = i+1; j < n; j++) {
+//                sum = sum + nums[i];
+                if (nums[i]+nums[j] == target){
+                    return new int[] {i,j};
+                }
             }
-        }
+        }return nums;
     }
 }
