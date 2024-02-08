272b1346c8d38e933a60c7b91f96ff62c93d6b19
Shashi raj <shashiraj13457@gmail.com>
2024-02-08 23:50:44 +0530

    SortColors

--- Medium/SortColors.java
+++ Medium/SortColors.java
@@ -10,7 +10,8 @@ public class SortColors {
 
 //    Brute-force Approach
     static int[] sortColors(int[] nums) {
-            // int initial = nums[0];.
+        //red,green and yellow
+//             int initial = nums[0];
             int temp = 0;
             for(int i = 0; i<nums.length-1;i++){
                 for(int j = i+1;j<nums.length;j++){
